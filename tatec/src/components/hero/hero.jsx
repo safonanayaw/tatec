99902be65144d019cc9d4385793fa8c7d5fd9948
safonanayaw <safoasare17@gmail.com>
2025-06-20 22:15:22 +0100

    Build Hero component.

--- tatec/src/components/hero/hero.jsx
+++ tatec/src/components/hero/hero.jsx
@@ -1,17 +1,30 @@
 import "./hero.css";
+import bannerPng from "../../assets/images/banner.png";
+import bannerPng2 from "../../assets/images/banner@2x.png";
+import bannerWebp from "../../assets/images/banner.webp";
+import bannerWebp2 from "../../assets/images/banner@2x.webp";
 
 const Hero = () => {
     return (
         <section className="block block--dark block--skewed--left">
             <div className="container grid grid--1x2">
                 <header className="block__header hero__content">
-                <h1 className="block__heading">Business Website & Software Development</h1>
-                <p className="hero__tagline">Consultancy for small business</p>
-                <a href="" class="btn btn--stretched btn--accent">Get Started</a>
+                    <h1 className="block__heading">Business Website & Software Development</h1>
+                    <p className="hero__tagline">Consultancy for small business</p>
+                    <div class="hero__btn">
+                    <a href="#" className="btn btn--stretched btn--accent">Get In Touch</a>
+                    <a href="#" className="btn btn--white hero__second-btn">See our Work</a>
+                    </div>
+
                 </header>
+                <picture>
+                    <source type="image/webp" srcSet={`${bannerWebp} 1x, ${bannerWebp2} 2x`} />
+                    <source type="image/png" srcSet={`${bannerPng} 1x, ${bannerPng2} 2x`} />
+                    <img className="hero__image" src={bannerPng} alt="Business Banner" />
+                </picture>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Hero;
